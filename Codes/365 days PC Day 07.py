754290d64ff6003a0643968981af1b61e25f451c
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-01-12 00:23:31 -0600

    Update 365 days PC Day 07.py

--- Codes/365 days PC Day 07.py
+++ Codes/365 days PC Day 07.py
@@ -6,3 +6,14 @@ Created on Sat Jan  7 23:17:45 2023
 @author: boladipo
 """
 
+
+import plotly.graph_objects as go
+
+
+fig = go.Figure(go.Treemap(
+    
+    labels = ["A", "B", "C", "D", "E", "F", "G", "H", "I"],
+    parents = ["", "A", "A", "C", "C", "A", "A", "G", "A"] ))
+
+
+fig.show()

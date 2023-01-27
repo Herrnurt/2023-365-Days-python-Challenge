00c930b57854ce8573f3c69e4a2bbd6fe79c093f
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-01-26 23:25:53 -0600

    Add files via upload

---
+++ Codes/365 days PC Day 26.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 26 23:12:17 2023
+
+@author: boladipo
+"""
+
+# Line Continuation charaters in Python
+
+print("I Know Python and its very easy for everyone")
+
+
+
+# In python, a backslash (\) is a continuation character
+
+print("I know Python abd \
+      its very easy for everyone")
+      
+      
+      
+      
+      
+print("I know Python abd \
+its very easy for everyone")      

a46d99a4fa34d72bc10fabf9987ebf7172503806
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-01-27 21:31:13 -0600

    Update 365 days PC Day 26.py

--- Codes/365 days PC Day 26.py
+++ Codes/365 days PC Day 26.py
@@ -23,3 +23,21 @@ print("I know Python abd \
       
 print("I know Python abd \
 its very easy for everyone")      
+
+
+
+
+from forex_python.converter import CurrencyRates
+
+c = CurrencyRates()
+
+amount = int(input("ENter the amount: "))
+from_currency = input("From Currency: ").upper()
+to_currency = input("To Currency: ").upper()
+
+print(from_currency, "to", to_currency)
+
+result = c.convert(from_currency, to_currency, amount)
+    
+print(result)
+    

1f69f17f37e7e3829cbdb9a99496b209d3851b1d
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-02-04 20:31:24 -0600

    Add files via upload

---
+++ Codes/365 days PC day 35.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  4 20:05:13 2023
+
+@author: boladipo
+"""
+
+## Create Font Art using Python
+
+import pyfiglet
+
+font = pyfiglet.figlet_format('Python Coding')
+print(font)

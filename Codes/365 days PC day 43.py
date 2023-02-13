d5c0c66bf06d3434b9f9b078abfca33f1c5d989c
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-02-12 22:46:30 -0600

    Add files via upload

---
+++ Codes/365 days PC day 43.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 12 22:29:15 2023
+
+@author: boladipo
+"""
+
+##  Search Anything in Python
+
+
+import pywhatkit as kit
+
+search_item = (input('Enter the topic :'))
+
+
+kit.info(search_item, 100)

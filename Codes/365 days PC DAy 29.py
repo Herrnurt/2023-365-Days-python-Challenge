55cbb8680c3abc952bc7d186f888ce924319402d
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-02-24 23:52:50 -0600

    Add files via upload

---
+++ Codes/365 days PC DAy 29.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 29 21:28:08 2023
+
+@author: boladipo
+"""
+
+##
+
+def ip_address(address):
+    new_address = ""
+    split_address = address.split(".")
+    seperator = '[.]'
+    new_address = separator.join(split_address)
+    return new_address
+
+Ipad = input("Enter your IP aadrees :")
+ipaddress = ip_address(IPad)
+print(ipadassress)

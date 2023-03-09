0717a41be611b75347ce96ca7be3325711bacb52
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-03-08 23:43:26 -0600

    Add files via upload

---
+++ Codes/365 days PC day 67.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  8 23:34:10 2023
+
+@author: boladipo
+"""
+
+# Python program to add two numbers
+
+
+
+
+
+number1 = input("first number : ")
+number2 = input("Second number: ")
+
+
+
+
+# Adding two numbers
+# User might also enter float numbers
+
+
+sum = float(number1) + float(number2)
+
+
+
+# Display the sum
+# will print value in float
+print(f"The sum of {number1} and {number2} is {sum}")

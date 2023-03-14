4b2a424103626f3633e3b64a56bf1168bf4de9c2
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-03-13 23:51:24 -0500

    Add files via upload

---
+++ Codes/365 days PC day 72.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 13 23:33:41 2023
+
+@author: boladipo
+"""
+
+
+
+
+## Bitwise Operations on Integer Types
+
+x = 8
+y = 3
+
+print(x|y)
+
+print(x^y)
+
+print(x&y)
+
+
+print(x<<y)
+
+
+print(x>>y)
+
+
+print(-x)

8894a145093e936e1dc66dc082c091b33f283b9f
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-03-18 23:53:18 -0500

    Add files via upload

---
+++ Codes/365 days PC day 77.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 18 23:52:45 2023
+
+@author: boladipo
+"""
+
+## Python porgram to print EMojis
+
+import emoji
+
+
+print(emoji.emojize(":Nigeria:"))
+print(emoji.emojize(":books:"))
+print(emoji.emojize(":red_heart:"))
+print(emoji.emojize(":hibiscus:"))
+print(emoji.emojize(":rose:"))
+print(emoji.emojize(":baby:"))

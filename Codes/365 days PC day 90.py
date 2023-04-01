2eb255c06810bde17848ccdb0654d7b3b4c94e01
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-03-31 23:45:19 -0500

    Add files via upload

---
+++ Codes/365 days PC day 90.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 31 23:40:27 2023
+
+@author: boladipo
+"""
+
+
+
+
+
+
+## How to Print a whole Calendar in python
+
+
+
+
+
+
+
+from calendar import*
+
+
+year = int(input("Enter year : "))
+
+print(calendar(year, 2, 1, 8, 4))

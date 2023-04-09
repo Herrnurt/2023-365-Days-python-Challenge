7f3f458c3cde552e2620fa5b61c3b282a8dbffcb
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-04-08 23:43:08 -0500

    Add files via upload

---
+++ Codes/365 days PC day 97.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  7 23:23:48 2023
+
+@author: boladipo
+"""
+
+
+
+## QR Code generation using Python
+
+
+
+
+import pyqrcode
+from PIL import Image
+
+
+link = input("Enter anything to generate QR: ")
+
+
+qr_code = pyqrcode.create(link)
+qr_code.png("QRCode.png", scale=4)
+
+Image.open("QRCode.png")
+
---
+++ Codes/365 days PC day 98.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  8 23:30:37 2023
+
+@author: boladipo
+"""
+
+## Convert Decimal into other number using Python
+
+
+
+
+
+
+# Python program to convert decimal into other systems
+
+dec = int(input("ENter a Decimal Number: "))
+
+
+
+
+# decimla to binary
+print(bin(dec), "in Binary.")
+
+
+# decimal to octal
+print(oct(dec), "in Octal.")
+
+
+
+# decimal to Hexadecimal
+print(hex(dec), "in Hexadecimal.")
+
+
+
+
+

15425899ba5d38f71703bbce7fcb18ea8559c4df
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-04-13 23:46:24 -0500

    Add files via upload

---
+++ Codes/365 days PC day 103.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 13 23:42:38 2023
+
+@author: boladipo
+"""
+
+
+
+
+
+
+## Where Maths doesn't work again
+
+
+
+# Guess the output in this case?
+
+
+a = (1 << 53) + 1
+
+
+
+print(a + 1.0 > a)

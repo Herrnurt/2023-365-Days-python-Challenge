31fb409fd16a3e0591bb1937d7c4dacdba5ba083
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-04-14 23:49:37 -0500

    Add files via upload

---
+++ Codes/365 days PC day 104.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 14 23:46:11 2023
+
+@author: boladipo
+"""
+
+
+
+## Lazy Operators --- Python 
+
+
+
+
+# Guest the output in this case
+print(all([]))
+
+
+# guess the output in this case?
+print(any([]))

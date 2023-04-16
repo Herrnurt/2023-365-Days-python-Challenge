c3a42f34e5d427082ab42b87baa59eb45cc9601a
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-04-15 23:45:25 -0500

    Add files via upload

---
+++ Codes/365 days PC day 105.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 15 23:28:16 2023
+
+@author: boladipo
+"""
+
+
+
+
+## Round function Python
+
+
+
+print(round(9/2))
+
+print(round(7/2))
+
+
+print(round(3/2))

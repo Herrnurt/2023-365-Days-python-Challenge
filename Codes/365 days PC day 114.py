f290d8f8272cc4098dd8d107bf9c2723cb6af1de
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-04-24 23:53:42 -0500

    Add files via upload

---
+++ Codes/365 days PC day 114.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 24 23:50:12 2023
+
+@author: boladipo
+"""
+
+
+
+# Careful with Chained operations
+
+(False == False) in [False]
+
+
+
+False == (False in [False])
+
+
+
+False == False in [False]

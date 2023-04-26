6309d3826170e36eb5af93dd0cbe0ba2d23b1866
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-04-25 23:43:11 -0500

    Add files via upload

---
+++ Codes/365 days PC day 115.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 25 23:32:21 2023
+
+@author: boladipo
+"""
+
+
+## Create a Progress Bar Python 
+
+
+import time
+for i in range(10):
+    
+    time.sleep92
+
+
+
+from tqdm import tqdm
+
+import time
+
+
+for i in tqdm(range(10)):
+    
+    time.sleep(2)
+
+

1b77a5074816b14e46f011a02e2a0ef1d2c54599
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-04-28 23:28:43 -0500

    Add files via upload

---
+++ Codes/365 days PC day 118.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 28 23:24:15 2023
+
+@author: boladipo
+"""
+
+
+
+
+
+
+## Create Multiple Libraries in one Libe
+
+
+
+
+
+import numpy as np
+import pandas as pd 
+import tensorflow as tf
+
+
+
+import numpy as np, pandas as pd, tensorflow as tf
+
+
+

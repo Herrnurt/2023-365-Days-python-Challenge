5cd12958ae3c629ac93ca3cdb94abe517b8d5942
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-05-03 23:49:44 -0500

    Add files via upload

---
+++ Codes/365 days PC day 123.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  3 23:43:32 2023
+
+@author: boladipo
+"""
+
+
+
+
+## Infinity values in Python
+
+
+
+a = float("inf")
+
+b = float("-inf")
+
+print(a/b)
+print(a*b)
+print(a+b)
+print(a-b)

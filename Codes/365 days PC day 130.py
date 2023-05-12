aaa3d79d0f7ba3733a5fcc02f7f4f8b0cc0c431b
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-05-11 23:47:04 -0500

    Add files via upload

---
+++ Codes/365 days PC day 130.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 11 23:41:52 2023
+
+@author: boladipo
+"""
+
+
+
+
+## Magic Method Invocation in Python
+
+
+num = 50
+result1 = num + 10
+result2 = num.__add__(10)
+print(result1)
+print(result2)

1031b08a866dd8e9581911eaead694533a56ea25
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-06-03 23:53:42 -0500

    Add files via upload

---
+++ Codes/365 days PC day 148.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  3 23:41:04 2023
+
+@author: boladipo
+"""
+
+
+## Image COlor Extraction with Python
+
+
+
+
+from PIL import Image
+
+Image.open('./download.png')
+
+
